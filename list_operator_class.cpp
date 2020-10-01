b0de096dfd596442d9fd20737d0b971528f6cc2a
Matin-Modarresi <58595108+Matin-Modarresi@users.noreply.github.com>
2020-10-01 17:54:03 +0330

    main file

--- list_operator_class.cpp
+++ list_operator_class.cpp
@@ -1,132 +1,126 @@
+// class
 #include <iostream>
 #include <list>
-#include <iterator>
 using namespace std;
+///////////////////////////////////////////////////////////////////////
+class c
+{
+   friend ostream &operator<<(ostream &, const c &);
 
-class c{
-	friend ostream &operator << (ostream & , const c&);
-	
-	public:
-		int x;
-		int y;
-		 
-		c();
-		c( c&);
-		~c(){};
-		
-		c&    operator = (const c &);
-		int  operator == (const c &) const;
-		int  operator < (const c &) const;
-		
+   public:
+      int x;
+      int y;
+
+      c();
+      c(const c &);
+      ~c(){};
+      
+      c &operator=(const c &rhs);
+      int operator==(const c &rhs) const;
+      int operator<(const c &rhs) const;
 };
-
-c::c(){
-	
-	x=0;
-	y=0;
-}
-
- c::c( c &copyin)
+/////////////////////////////////////////////////////////
+c::c()   // Constructor
 {
-	x=copyin.x;
-	y=copyin.y;
+   x = 0;
+   y = 0;
 }
-
-ostream &operator<<(ostream &o ,const c &c)
+/////////////////////////////////////////////////////////
+c::c(const c &copyin)   // Copy constructor to handle pass by value.
+{                             
+   x = copyin.x;
+   y = copyin.y;
+}
+/////////////////////////////////////////////////////////
+ostream &operator<<(ostream &o, const c &c)
 {
-	o << c.x << ' ' << c.y;
-	
-	return o;
+   o << c.x << ' ' << c.y <<  endl;
+   return o;
 }
-
+/////////////////////////////////////////////////////////
 c& c::operator=(const c &k)
 {
-	this -> x = k.x;
-	this -> y = k.y;
-	
-	return *this;
+   this->x = k.x;
+   this->y = k.y;
+   return *this;
 }
-
-
-int c::operator==(const c &k ) const
+/////////////////////////////////////////////////////////
+int c::operator==(const c &k) const
 {
-	if(this->x == k.x)
-	 return 0;
-	 
-	 else return 0;
+   if( this->x != k.x) 
+       return 0;
+   if( this->y != k.y) 
+       return 0;
+       
+   return 1;
 }
-
-int c::operator < (const c &k) const
+/////////////////////////////////////////////////////////
+// This function is required for built-in STL list functions like sort
+int c::operator<(const c &k) const
 {
-	if(this->x == k.x && this->y == k.y)
-	 return 1;
-	 
-	 if (this->x==k.x && this->y < k.y)
-	 return 1;
-	 
-	 if(this->x < k.x)
-	 return 1;
-	 
-	 return 0;
-	 
+   if( this->x == k.x && this->y == k.y) 
+         return 1;
+         
+   if( this->x == k.x && this->y < k.y) 
+         return 1;
+         
+   if( this->x < k.x ) 
+         return 1;
+         
+   return 0;
 }
+/////////////////////////////////////////////////////////
 
-
-int main()
+main()
 {
    list<c> L;
-   
-   c t;
-   
-   t.x=1;   
-   t.y=2;  
-   L.push_back(t); //  1 2
-   
-   
+   c t ;
+
+   t.x=1;
+   t.y=2;
+   L.push_back(t);  // 1  2 
+
    t.x=3;
-   L.push_back(t);  // 1 2 -> 3 2
+   L.push_back(t);   //  1  2  ->  3  2  
    
    t.y=4;
-   L.push_back(t); // 1 2 -> 3 2 -> 3 4
-   
+   L.push_back(t);   //  1  2  ->  3  2  ->  3  4
+
    t.x=2;
    t.y=7;
-   L.push_back(t); // 1 2 -> 3 2 -> 3 4 -> 2 7   
+   L.push_back(t);   //  1  2  ->  3  2  ->  3  4 -> 2  7
    
    list<c>::iterator i;
+///
+   cout << "List: "<<endl ;
+   for(i=L.begin(); i != L.end(); ++i) 
+   		cout <<(*i).x <<" "<< (*i).y << endl; 
+   		
+   cout << endl << endl;      
+////
+   cout << "Sorted: " << endl;
+   L.sort();
+   for(i=L.begin(); i != L.end(); ++i) 
+            cout << *i; 
+   cout << endl;
+////
+   for(i=L.begin(); i != L.end(); )       
+       if( (*i).x == 2 ) 
+	         i = L.erase(i);       
+       else i++; 
+   cout << "delete: " << endl;
+   for(i=L.begin(); i != L.end(); ++i) 
+         cout << *i; 
+   cout << endl;
+ ////  	         
+   cout << "Show Reverse: " << endl;
+   list<c>::reverse_iterator ri;
+   for(ri=L.rbegin(); ri != L.rend(); ++ri) 
+              cout << *ri;                 
+   cout << endl;   
+			                          
    
-   cout << "List: " << endl;	
-   
-   for(i=L.begin() ; i!=L.end() ; i++)
-     cout << (*i).x << " " << (*i).y << endl;
-	 
-	 cout << "\n\n";
-	 
-	 cout << "Sorted: \n";
-	 L.sort();
-	 for(i=L.begin() ; i !=L.end() ; i++)
-	 cout << *i;
-	 
-	 cout << "\n";
-	 
-	 for(i=L.begin(); i!=L.end() ; i++)
-	   if((*i).x == 2)
-	   i = L.erase(i);
-	     
-	     else i++;
-	     
-	cout << "delete: " << endl;
-	for(i=L.begin() ; i!=L.end() ; i++)
-	 cout << *i;
-	 
-	cout << endl;
-	
-	cout << "Show Reverse: " << endl;
-	list<c>::reverse_iterator ri;
-	for(ri=L.begin() ; ri!=L.rend() ; ri++)
-	  cout << *ri ;
-	  
-	  cout << endl;
-	 
-	  
+
 }
+
+          
